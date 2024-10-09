b59c4d845ae7501116a7666b4e9b1e82ee94cb0c
krisbhoyt <krisb.hoyt@gmail.com>
2024-10-09 17:42:02 -0600

    Update roi_visualizer.py

--- roi_visualizer.py
+++ roi_visualizer.py
@@ -118,9 +118,9 @@ if submit_button:
 
         # Cost Efficiency Gain
         if processors_saved < 1:
-            total_labor_cost_savings = processors_saved * ap_processor_salary
-        else:
             total_labor_cost_savings = processors_saved / ap_processor_salary
+        else:
+            total_labor_cost_savings = processors_saved * ap_processor_salary
                                       
         return {
             "Projected Invoice Volume": annual_invoice_volume,
